1909650a03685cf74202c1f8ada7700720366108
Oliver Tang <olivertang@Olivers-MBP.hitronhub.home>
2018-08-07 17:31:38 -0700

    test will revert

--- dist/components/Board.js
+++ dist/components/Board.js
@@ -4,10 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _extends2 = require('babel-runtime/helpers/extends');
-
-var _extends3 = _interopRequireDefault(_extends2);
-
 var _getPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of');
 
 var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
@@ -78,9 +74,9 @@ var Board = function (_Component) {
     key: 'render',
     value: function render() {
       return _react2.default.createElement(
-        _reactRedux.Provider,
-        { store: this.store },
-        _react2.default.createElement(_BoardContainer2.default, (0, _extends3.default)({}, this.props, { id: this.id }))
+        'div',
+        null,
+        'hello board.js'
       );
     }
   }]);
