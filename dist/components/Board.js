abde4bca8d832b83fc7915b5029e371e99a9f20b
Oliver Tang <olivertang@Olivers-MBP.hitronhub.home>
2018-08-07 17:47:39 -0700

    Revert "with dist chanages"
    This reverts commit 89b0a74799ac1fe6889cb5d45fb8d7daa6e57360.

--- dist/components/Board.js
+++ dist/components/Board.js
@@ -4,6 +4,10 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends2 = require('babel-runtime/helpers/extends');
+
+var _extends3 = _interopRequireDefault(_extends2);
+
 var _getPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of');
 
 var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
@@ -74,9 +78,9 @@ var Board = function (_Component) {
     key: 'render',
     value: function render() {
       return _react2.default.createElement(
-        'div',
-        null,
-        ' hello world '
+        _reactRedux.Provider,
+        { store: this.store },
+        _react2.default.createElement(_BoardContainer2.default, (0, _extends3.default)({}, this.props, { id: this.id }))
       );
     }
   }]);
